5ee34e1aeceab73410f7d4e05ed7dd20c2018884
Malvina Kochkina <malvina@Malvinas-MacBook-Air.local>
2024-06-19 15:41:35 +0200

    adjust formatting

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿static void TreasureHunt()
-{
+﻿TreasureHunt();
+
+static void TreasureHunt()
+    {
     var gameRunning = true;
 
     while (gameRunning)
-    {
+        {
         var (gridRows, gridColumns) = GetGridSize();
         var grid = InitializeGrid(gridRows, gridColumns);
         var numberOfTreasures = (int)(gridRows * gridColumns * 0.1);
@@ -13,190 +15,239 @@
 
         var treasureCounter = 0;
         Console.Clear();
-        DisplayGrid(grid);
         PlaceItems(numberOfTreasures, '$', grid);
         PlaceItems(numberOfObstacles, '|', grid);
+        DisplayGrid(grid);
 
-        ConsoleKeyInfo keyInfo;
-        var gameActive = true;
+        RunTheGame(grid, numberOfTreasures, ref playerRow, ref playerColumn, ref monsterRow, ref monsterColumn,
+            ref treasureCounter);
 
-        while (gameActive && (keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
+        gameRunning = RestartTheGame();
+        }
+    }
+
+static void RunTheGame
+(
+    char[,] grid, int numberOfTreasures, ref int playerRow, ref int playerColumn, ref int monsterRow,
+    ref int monsterColumn, ref int treasureCounter
+)
+    {
+    ConsoleKeyInfo keyInfo;
+    var gameActive = true;
+
+    while (gameActive && (keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
         {
-            (playerRow, playerColumn, treasureCounter) =
-                MovePlayer(grid, playerRow, playerColumn, keyInfo, treasureCounter);
-            (monsterRow, monsterColumn) = MoveMonster(grid, playerRow, playerColumn, monsterRow, monsterColumn);
+        (playerRow, playerColumn, treasureCounter) =
+            MovePlayer(grid, playerRow, playerColumn, keyInfo, treasureCounter);
+        (monsterRow, monsterColumn) = MoveMonster(grid, playerRow, playerColumn, monsterRow, monsterColumn);
 
+        Console.Clear();
+        DisplayGrid(grid);
+        Console.WriteLine("Number of Treasures: " + numberOfTreasures);
+        Console.WriteLine("Treasure Counter: " + treasureCounter);
+
+        if (treasureCounter == numberOfTreasures)
+            {
             Console.Clear();
-            DisplayGrid(grid);
-            Console.WriteLine("Number of Treasures: " + numberOfTreasures);
-            Console.WriteLine("Treasure Counter: " + treasureCounter);
-
-            if (treasureCounter == numberOfTreasures)
-            {
-                Console.Clear();
-                Console.WriteLine($"You found all {numberOfTreasures} treasures and win the game!");
-                gameActive = false;
+            Console.WriteLine($"You found all {numberOfTreasures} treasures and win the game!");
+            gameActive = false;
             }
-            else if (monsterRow == playerRow && monsterColumn == playerColumn)
+        else if (monsterRow == playerRow && monsterColumn == playerColumn)
             {
-                Console.Clear();
-                Console.WriteLine("The monster caught you! Game over.");
-                gameActive = false;
+            Console.Clear();
+            Console.WriteLine("The monster caught you! Game over.");
+            gameActive = false;
             }
         }
-
-        Console.WriteLine("Press 'R' to restart the game or any other key to exit.");
-        var restartKey = Console.ReadKey(true).Key;
-        if (restartKey != ConsoleKey.R) gameRunning = false;
     }
-}
 
-static (int, int) GetGridSize()
-{
-    Console.WriteLine("Enter row and column (for example 5x5):");
-    var input = Console.ReadLine();
-    var parts = input.Split('x');
-    var gridRows = int.Parse(parts[0]);
-    var gridColumns = int.Parse(parts[1]);
-    return (gridRows, gridColumns);
-}
+static bool RestartTheGame()
+    {
+    Console.WriteLine("Press 'R' to restart the game or any other key to exit.");
+    var restartKey = Console.ReadKey(true).Key;
+    return restartKey == ConsoleKey.R;
+    }
+
+static (int height, int width) GetGridSize()
+    {
+    while (true)
+        {
+        Console.WriteLine("Enter row and column (for example 5x5):");
+        var input = Console.ReadLine();
+        var parts = input.Split('x');
+
+        if (parts.Length == 2 && int.TryParse(parts[0], out var gridRows) &&
+            int.TryParse(parts[1], out var gridColumns) && gridRows > 0 && gridColumns > 0)
+            {
+            return (height: gridRows, width: gridColumns);
+            }
+
+        Console.WriteLine(
+            "Invalid input. Please enter the grid size in the format RxC, where R and C are positive integers.");
+        }
+    }
 
 static char[,] InitializeGrid(int gridRows, int gridColumns)
-{
+    {
     var grid = new char[gridRows, gridColumns];
     for (var r = 0; r < gridRows; r++)
     for (var c = 0; c < gridColumns; c++)
+        {
         grid[r, c] = '.';
+        }
+
     return grid;
-}
+    }
 
 static int PlaceItems(int numberOfItems, char itemSymbol, char[,] grid)
-{
+    {
     var random = new Random();
     var gridRows = grid.GetLength(0);
     var gridColumns = grid.GetLength(1);
     for (var i = 0; i < numberOfItems; i++)
-    {
-        int row, column;
-        do
         {
+        int row;
+        int column;
+        do
+            {
             row = random.Next(gridRows);
             column = random.Next(gridColumns);
-        } while (grid[row, column] == '$' || grid[row, column] == 'P');
+            } while (grid[row, column] == '$' || grid[row, column] == 'P');
 
         grid[row, column] = itemSymbol;
-    }
+        }
 
     return numberOfItems;
-}
+    }
 
-static (int, int) PlacePlayer(char[,] grid)
-{
+static (int row, int column) PlacePlayer(char[,] grid)
+    {
     var random = new Random();
     var gridRows = grid.GetLength(0);
     var gridColumns = grid.GetLength(1);
     int playerRow, playerColumn;
     do
-    {
+        {
         playerRow = random.Next(gridRows);
         playerColumn = random.Next(gridColumns);
-    } while (grid[playerRow, playerColumn] == '$' || grid[playerRow, playerColumn] == 'M');
+        } while (grid[playerRow, playerColumn] == '$' || grid[playerRow, playerColumn] == 'M');
 
     grid[playerRow, playerColumn] = 'P';
-    return (playerRow, playerColumn);
-}
+    return (row: playerRow, column: playerColumn);
+    }
 
-static (int, int) PlaceMonster(char[,] grid)
-{
+static (int row, int column) PlaceMonster(char[,] grid)
+    {
     var random = new Random();
     var gridRows = grid.GetLength(0);
     var gridColumns = grid.GetLength(1);
-    int monsterRow, monsterColumn;
+    int monsterRow;
+    int monsterColumn;
     do
-    {
+        {
         monsterRow = random.Next(gridRows);
         monsterColumn = random.Next(gridColumns);
-    } while (grid[monsterRow, monsterColumn] == '$' || grid[monsterRow, monsterColumn] == 'P');
+        } while (grid[monsterRow, monsterColumn] == '$' || grid[monsterRow, monsterColumn] == 'P');
 
     grid[monsterRow, monsterColumn] = 'M';
-    return (monsterRow, monsterColumn);
-}
+    return (row: monsterRow, column: monsterColumn);
+    }
 
-static (int, int, int) MovePlayer(char[,] grid, int playerRow, int playerColumn, ConsoleKeyInfo keyInfo,
-    int treasureCounter)
-{
+static (int row, int column, int treasureCounter) MovePlayer
+    (char[,] grid, int playerRow, int playerColumn, ConsoleKeyInfo keyInfo, int treasureCounter)
+    {
     var newRow = playerRow;
     var newColumn = playerColumn;
 
     switch (keyInfo.Key)
-    {
-        case ConsoleKey.UpArrow:
-            newRow = playerRow > 0 ? playerRow - 1 : playerRow;
-            break;
-        case ConsoleKey.DownArrow:
-            newRow = playerRow < grid.GetLength(0) - 1 ? playerRow + 1 : playerRow;
-            break;
-        case ConsoleKey.LeftArrow:
-            newColumn = playerColumn > 0 ? playerColumn - 1 : playerColumn;
-            break;
-        case ConsoleKey.RightArrow:
-            newColumn = playerColumn < grid.GetLength(1) - 1 ? playerColumn + 1 : playerColumn;
-            break;
-    }
+        {
+            case ConsoleKey.UpArrow:
+                newRow = playerRow > 0 ? playerRow - 1 : playerRow;
+                break;
+            case ConsoleKey.DownArrow:
+                newRow = playerRow < grid.GetLength(0) - 1 ? playerRow + 1 : playerRow;
+                break;
+            case ConsoleKey.LeftArrow:
+                newColumn = playerColumn > 0 ? playerColumn - 1 : playerColumn;
+                break;
+            case ConsoleKey.RightArrow:
+                newColumn = playerColumn < grid.GetLength(1) - 1 ? playerColumn + 1 : playerColumn;
+                break;
+        }
 
     if (grid[newRow, newColumn] != '|')
-    {
-        if (grid[newRow, newColumn] == '$') treasureCounter++;
+        {
+        if (grid[newRow, newColumn] == '$')
+            {
+            treasureCounter++;
+            }
 
         grid[playerRow, playerColumn] = '.';
         grid[newRow, newColumn] = 'P';
         playerRow = newRow;
         playerColumn = newColumn;
+        }
+
+    return (row: playerRow, column: playerColumn, treasureCounter);
     }
 
-    return (playerRow, playerColumn, treasureCounter);
-}
-
-static (int, int) MoveMonster(char[,] grid, int playerRow, int playerColumn, int monsterRow, int monsterColumn)
-{
+static (int row, int column) MoveMonster
+    (char[,] grid, int playerRow, int playerColumn, int monsterRow, int monsterColumn)
+    {
     var random = new Random();
     var monsterNewRow = monsterRow;
     var monsterNewColumn = monsterColumn;
 
     if (random.Next(2) == 0)
-    {
-        if (playerRow < monsterRow) monsterNewRow--;
-        else if (playerRow > monsterRow) monsterNewRow++;
-    }
+        {
+        if (playerRow < monsterRow)
+            {
+            monsterNewRow--;
+            }
+        else if (playerRow > monsterRow)
+            {
+            monsterNewRow++;
+            }
+        }
     else
-    {
-        if (playerColumn < monsterColumn) monsterNewColumn--;
-        else if (playerColumn > monsterColumn) monsterNewColumn++;
-    }
+        {
+        if (playerColumn < monsterColumn)
+            {
+            monsterNewColumn--;
+            }
+        else if (playerColumn > monsterColumn)
+            {
+            monsterNewColumn++;
+            }
+        }
 
     if (monsterNewRow >= 0 && monsterNewRow < grid.GetLength(0) && monsterNewColumn >= 0 &&
         monsterNewColumn < grid.GetLength(1))
-        if (grid[monsterNewRow, monsterNewColumn] != '|' && grid[monsterNewRow, monsterNewColumn] != '$')
         {
+        if (grid[monsterNewRow, monsterNewColumn] != '|' && grid[monsterNewRow, monsterNewColumn] != '$')
+            {
             grid[monsterRow, monsterColumn] = '.';
             grid[monsterNewRow, monsterNewColumn] = 'M';
             monsterRow = monsterNewRow;
             monsterColumn = monsterNewColumn;
+            }
         }
 
-    return (monsterRow, monsterColumn);
-}
+    return (row: monsterRow, column: monsterColumn);
+    }
 
 static void DisplayGrid(char[,] grid)
-{
+    {
     var rows = grid.GetLength(0);
     var columns = grid.GetLength(1);
 
     for (var r = 0; r < rows; r++)
-    {
-        for (var c = 0; c < columns; c++) Console.Write(grid[r, c] + " ");
-        Console.WriteLine();
-    }
-}
+        {
+        for (var c = 0; c < columns; c++)
+            {
+            Console.Write(grid[r, c] + " ");
+            }
 
-TreasureHunt();
+        Console.WriteLine();
+        }
+    }
